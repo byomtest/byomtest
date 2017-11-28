0421212b2ad6529a48630777d1ec96aa3a6ea10d
Myob test <nobody@nowhere.com>
2017-11-28 22:15:06 +1100

    added failing test class

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class ApplicationTest {
+    @Test
+    public void shouldFail() {
+        assertFalse(true);
+    }
+}
